037b66b7d246af563226116bb8eefb1f0a82b65b
Felipe Silva <felipe.silva@cern.ch>
2026-04-29 02:11:11 +0200

    Add mini2mini Nano muon ID recomputation customization

--- Configuration/StandardSequences/python/REMINI_cff.py
+++ Configuration/StandardSequences/python/REMINI_cff.py
@@ -5,3 +5,9 @@ patAlgosToolsTask = cms.Task()
 from PhysicsTools.PatAlgos.slimming.miniAODFromMiniAOD_tools import miniAODFromMiniAOD_customizeAllData as miniAOD_customizeAllData
 from PhysicsTools.PatAlgos.slimming.miniAODFromMiniAOD_tools import miniAODFromMiniAOD_customizeAllMC as miniAOD_customizeAllMC
 
+
+def fixMini2MiniMuonIDs(process):
+    process.slimmedMuonsUpdated.recomputeSoftMuonMvaRun3 = cms.bool(True)
+    process.slimmedMuonsUpdated.recomputeMuonBasicSelectors = cms.bool(True)
+    return process
+
